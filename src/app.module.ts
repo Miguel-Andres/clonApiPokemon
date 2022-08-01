42830f30aaeb030e3189e747151a3edb5a0bd8f7
Miguel Andres <miguel_3715@hotmail.com>
2022-08-01 11:51:21 -0300

    custom provider http

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { SeedModule } from './seed/seed.module';
     CommonModule,
 
     SeedModule,
+
   ],
 })
 export class AppModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
+
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse, SmallPokemon } from './interfaces/poke-response.interface';
 
@@ -9,18 +10,20 @@ import { PokeResponse, SmallPokemon } from './interfaces/poke-response.interface
 @Injectable()
 export class SeedService {
 
-  private readonly axios : AxiosInstance = axios
+
 
 constructor(
   @InjectModel(Pokemon.name)
   private readonly pokemonModel: Model<Pokemon>,
+
+  private readonly http:AxiosAdapter ,
 ){}
   
  async executeSeed (){
     // delete where * from pokemons
   await this.pokemonModel.deleteMany({})
 
-   const {data} =await this.axios.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=10")
+   const data  =await this.http.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=30")
 
     
 
